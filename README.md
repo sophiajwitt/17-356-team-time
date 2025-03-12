1bd541d2112deb594408d2ab883d87d45ec2e3ee
hsharriman <hwei.shin.harriman@gmail.com>
2025-03-12 18:04:33 -0400

    set up backend and frontend docker containers, convert frontend to TS+React and installed tailwind

---
+++ README.md
@@ -0,0 +1,12 @@
+# Research Social App
+
+## Building and Running
+
+`frontend` contains the frontend files
+`backend` contains Node.js backend files
+`db` is empty for now, but will contain files related to the PostgresQL database, including schema information.
+
+To start the docker containers:
+
+1. Run `docker-compose build`
+2. Run `docker-compose up` to start the frontend and backend processes.
---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
