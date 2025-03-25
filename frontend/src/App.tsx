432afaf02c890dc7c25fc51c2e412de27aca9136
smithlauren910 <77132057+smithlauren910@users.noreply.github.com>
2025-03-25 17:05:54 -0400

    Creates frontend for landing page and user registration form (#34)
    # What changed
    
    Resolves issue #18 
    Creates a landing page with a button to the user registration form
    Creates the user registration form with text input and submission
    button.
    
    # Why this change
    
    We will need a landing page moving forward for users to choose between
    logging in and registering as a new user. The user registration form is
    a crucial feature to ensure people can create an account and use our
    website.
    
    # How was it tested
    
    From the frontend folder, `npm run dev` was executed. The changes were
    checked manually at localhost:8080 to ensure the user registration and
    form submission (database not yet connected) buttons work as expected.
    Form validation (ex. completion, matching passwords, email validation)
    were also checked.
    
    # Screenshots
    
    Landing Page 
    <img width="1434" alt="image"
    src="https://github.com/user-attachments/assets/a6ebca8c-862e-4a30-985b-893220fe8e08"
    />
    
    User Registration Form
    <img width="1437" alt="image"
    src="https://github.com/user-attachments/assets/877d178c-6618-4543-ba21-1d2de4be95ba"
    />
    
    ---------
    
    Signed-off-by: Lauren Smith <smithlauren940@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Registration from "./pages/Registration";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bold text-3xl text-blue-500">Vite + React</h1>
-      <div className="">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="">Click on the Vite and React logos to learn more</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Registration />} />
+      </Routes>
+    </Router>
   );
 }
 
