54354765fcf40ce8a27f744548e50fcc20409796
dksung101 <117541491+dksung101@users.noreply.github.com>
2025-03-28 11:26:35 -0400

    Refactor: Deleted Users table and modified Profiles table in db schema (#51)
    Resolves #47 
    
    # What changed
    
    Removed Users table in database schema. Moved fields from User table to
    Profile table as well as userId field. Lastly, removed profileId field
    from Profiles table.
    
    # Why this change
    
    Users table was redundant and Profiles table had profileId and userId
    both referring to the same string.
    
    # How was it tested
    
    N/A: Quick database schema update for our reference.

---
+++ backend/db/README.md
@@ -0,0 +1,14 @@
+# Database
+
+## Setup
+
+### Install dependencies
+
+```bash
+npm install
+```
+
+### Create .env file with AWS credentials
+
+1. Create Access Key for individual users: https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html
+2. Fill in required fields in .env file: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION=us-east-2
--- backend/db/schemas.ts
+++ backend/db/schemas.ts
@@ -1,19 +1,11 @@
 /**
- * The basic User model
+ * Profile model - extends User since it contains duplicate information
  */
-export interface User {
-  userId: string;
+export interface Profile {
+  userId: string; // Primary key
   firstName: string;
   lastName: string;
   email: string;
-  createdAt: string;
-}
-
-/**
- * Profile model - extends User since it contains duplicate information
- */
-export interface Profile extends User {
-  profileId: string; // Primary key
   createdAt: string;
   updatedAt?: string;
   phone?: string; // Optional
@@ -24,6 +16,5 @@ export interface Profile extends User {
 
 // Table names as constants for easier referencing
 export enum TableNames {
-  USERS = "Users",
   PROFILES = "Profiles",
 }
